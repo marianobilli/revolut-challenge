84827927ca8117b9bf301ed8a75c12d052c9dc73
marianobilli <marianobilli@protonmail.com>
2021-10-11 01:24:16 +0200

    feat: add necesart tags to subnets required for alb and asg controllers

--- terraform/05_subnets.tf
+++ terraform/05_subnets.tf
@@ -1,12 +1,14 @@
 resource "aws_subnet" "public" {
   for_each = local.network.public
 
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = each.value
-  availability_zone = each.key
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value
+  availability_zone       = each.key
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "revolut-public-${each.key}"
+    Name                            = "revolut-public-${each.key}"
+    "kubernetes.io/cluster/revolut" = "shared"
+    "kubernetes.io/role/elb"        = "1"
   }
 }
